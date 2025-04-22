85ce625a74b2cfa477d0dff1fa240eac89268afb
Rayssa <rayssa.senaiii@gmail.com>
2025-04-22 14:17:20 -0300

    PromoCard mockup

--- 08-devSteam/src/components/Promotion.jsx
+++ 08-devSteam/src/components/Promotion.jsx
@@ -5,8 +5,10 @@ const Promotion = () => {
   return (
     <div id="promotion">
         <h2>Promoções</h2>
-    <div id='itensPromo'>
+    <div id='itensPromo' className='d-flex flex-wrap gap-4 justify-content-around'>
     {/* inserir os cards de promoção */}
+    <PromoCard />
+    <PromoCard />
     <PromoCard />
     </div>
     </div>
