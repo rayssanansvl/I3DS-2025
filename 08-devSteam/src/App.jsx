514ec13b56fffc3a82ddd9bef925374579e763db
Rayssa <rayssa.senaiii@gmail.com>
2025-04-22 10:57:11 -0300

    Iniciado o item de promoção

--- 08-devSteam/src/App.jsx
+++ 08-devSteam/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header'
+import Promotion from './components/Promotion'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Promotion />
     </>
   )
 }
---
+++ 08-devSteam/src/components/Promotion.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Promotion = () => {
+  return (
+    <div id="promotion">
+        <h2>Promoções</h2>
+    <div id='itensPromo'>
+
+    </div>
+    </div>
+  )
+}
+
+export default Promotion
