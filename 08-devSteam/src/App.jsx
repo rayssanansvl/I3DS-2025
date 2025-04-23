6bf403264eb81d29dcbbb883dfec7bc919e73363
Rayssa <rayssa.senaiii@gmail.com>
2025-04-23 08:05:23 -0300

    Entendendo o processo de adicionar item no header

--- 08-devSteam/src/App.jsx
+++ 08-devSteam/src/App.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Promotion from "./components/Promotion";
 
 function App() {
+  const[contador, setContador] = useState(0);
+
+  const handleAddCarrinho = () => {
+    setContador(contador + 1);
+}
+
   return (
     <>
       <Header contadorJogos={2} />
       <Promotion />
+      <button onClick={handleAddCarrinho}>+1</button>
     </>
   );
 }
